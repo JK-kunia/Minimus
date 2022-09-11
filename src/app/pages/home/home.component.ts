d2b86795a84935e56f1e92dac30f0f87203ecd37
jinponpo <jk.kunia+github@gmail.com>
2022-09-11 23:16:57 +0900

    homeコンポーネント修正

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FbService } from '../../services/fb/fb.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  cities;
 
-  ngOnInit() {}
+  constructor(public fb: FbService) {}
+
+  ngOnInit() {
+    this.cities = this.fb.getCities();
+  }
 }
